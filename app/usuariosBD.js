6fc291d6e17b890d34a40e162af81684a64123bd
Jhonfer Valencia <valenciajhonfer@gmail.com>
2022-04-18 20:59:03 -0500

    first commit

---
+++ app/usuariosBD.js
@@ -0,0 +1,12 @@
+let usuariosBD = [
+    {
+        nombre: "Junior",
+        apellido: "López",
+        correo: "Juniorlopez3@gmail.com",
+        photo: "https://i.ibb.co/j3Ppyjq/Ellipse-10-jr18.png",
+        usuario: "juniorlopez",
+        contrasenia: "1234"
+    },
+]
+
+export default usuariosBD
